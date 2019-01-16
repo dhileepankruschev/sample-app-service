9eaf2ca617fdefd4078bf58d523b06c90baaec61
Kruschev <244898@cognizant.com>
2019-01-16 16:56:25 +0530

    Initial Commit

---
+++ src/main/java/com/fse/wttracker/SampleAppServiceApplication.java
@@ -0,0 +1,14 @@
+package com.fse.wttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppServiceApplication.class, args);
+	}
+
+}
+
